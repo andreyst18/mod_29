f4e272157888e7419c0b2095a2a7d2988e535dac
Andrey Strelin <andrejmax@yandex.ru>
2021-09-29 23:05:19 +0400

    upload project files

---
+++ application/views/contacts_view.php
@@ -0,0 +1,6 @@
+<h1>Контакты</h1>
+<p>
+skypeid: fffrrr<br/>
+email: mpmp@gmail.com<br/>
+<br/>
+
---
+++ application/views/main_view.php
@@ -0,0 +1,7 @@
+<h1>Добро пожаловать!</h1>
+<p>
+<img src="/images/office-small.jpg" align="left" >
+<a href="/">Главная</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Deserunt error aut ea placeat distinctio magni esse nam, officiis consequuntur quae corporis sequi unde, quaerat accusamus porro voluptates numquam, 
+tenetur consectetur.<a href="/">Главная</a>.
+</p>
